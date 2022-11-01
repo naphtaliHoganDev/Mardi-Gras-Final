ce7605e86a210bb9e6b1874033aebedc6a17d11e
Naphtali Hogan <113341367+Detsen99@users.noreply.github.com>
2022-11-01 15:57:58 +0200

    Update contact.js

--- Mardi Gras/JS/contact.js
+++ Mardi Gras/JS/contact.js
@@ -20,28 +20,50 @@ inputs.forEach((input) => {
 const firstNameInput = document.getElementById("name");
 const lastNameInput = document.getElementById("lastname");
 const emailInput = document.getElementById("email1");
+const msg = document.getElementById("message1");
 const form = document.getElementById("contactus");
 const nameError = document.getElementById("name-feedback");
 const lastNameError = document.getElementById("last-name-feedback");
 const emailError = document.getElementById("email-feedback");
+const messageError = document.getElementById("message1Error");
 
 form.addEventListener("submit", (e) => {
     let note = [];
     let note2 = [];
     let note3 = [];
+    let note4 = [];
+
 
     let name = firstNameInput.value;
     let lastName = lastNameInput.value;
     let emailV = emailInput.value;
+    let msgInput = msg.value;
     if (name.length === 0) {
-        note.push("name is required");
+        note.push("First name is required!");
+    }
+    if (name.length >= 2) {
+        note.push("");
     }
     if (lastName.length === 0) {
-        note2.push("last name is required");
+        note2.push("Last name is required!");
     }
-
+    if (lastName.length >= 1) {
+        note2.push("");
+    }
     if (!validateEmail(emailV)) {
-        note3.push("email is invalid");
+        note3.push("Email is invalid!");
+    }
+    if (!validateEmail(emailV) === !validateEmail(emailV)) {
+        note3.push("");
+    }
+    if (msgInput.length === 0) {
+        note4.push("Please add a message!");
+    }
+    if (msgInput.length <= 10) {
+        note4.push("A minimum of 10 characters is required.");
+    }
+    if (msgInput.length >= 10) {
+        note4.push("");
     }
     if (note.length > 0) {
         e.preventDefault();
@@ -53,7 +75,11 @@ form.addEventListener("submit", (e) => {
     }
     if (note3.length > 0) {
         e.preventDefault();
-        emailError.innerText = note3.join(", ");
+        emailError.innerText = note3.join(" ");
+    }
+    if (note4.length > 0) {
+        e.preventDefault();
+        messageError.innerText = note4.join(", ");
     }
 });
 
@@ -66,5 +92,7 @@ function validateEmail(email) {
         (email.endsWith(".com") ||
             email.endsWith(".co.il") ||
             email.endsWith(".org"))
+
     );
+
 }
